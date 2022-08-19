5090e1504c9d05f0dcf7ac9a749c9bce11c214af
Hwangwonjin <ghkddnjs1269@gmail.com>
2022-08-19 19:47:56 +0900

    Backjoon

--- Backjoon/src/Stape01/_03.java
+++ Backjoon/src/Stape01/_03.java
@@ -9,13 +9,16 @@ import java.util.Scanner;
  * 
  */
 public class _03 {
-public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	
-	int a = sc.nextInt();
-	int b = sc.nextInt();
-	
-	
-	System.out.println(a+b);
-}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println(a+b);
+	}
+
 }
--- Backjoon/src/Stape01/_07.java
+++ Backjoon/src/Stape01/_07.java
@@ -5,13 +5,22 @@ import java.util.Scanner;
 /*
  * 날짜 : 2022/08/19
  * 이름 : 황원진
- * 내용 : 백준 1단계 7 문제 A/B
+ * 내용 : 백준 1단계 7 문제 사칙연산
  * 
  */
 public class _07 {
 	public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	
+	int a = sc.nextInt();
+	int b = sc.nextInt();
 	
-}
+	System.out.println(a+b);
+	System.out.println(a-b);
+	System.out.println(a*b);
+	System.out.println(a/b);
+	System.out.println(a%b);
+	
+	
+	}
 }
