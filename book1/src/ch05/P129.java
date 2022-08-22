92cb4b36ab047638f74d4dd3a52681182bb8d919
Hwangwonjin <ghkddnjs1269@gmail.com>
2022-08-22 19:18:44 +0900

    book1

---
+++ book1/src/ch05/P129.java
@@ -0,0 +1,15 @@
+package ch05;
+/*
+ * 날짜 : 2022/08/22
+ * 이름 : 황원진
+ * 내용 : 학생 클래스 만들기
+ * 
+ */
+public class P129 {
+
+	int studentID;
+	String studentName;
+	int grade;
+	String address;
+
+}
---
+++ book1/src/ch06/P173.java
@@ -0,0 +1,10 @@
+package ch06;
+
+public class P173 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
