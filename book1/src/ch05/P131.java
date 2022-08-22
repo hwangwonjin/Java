79bb2cdf75a3ad8b61cefeb36c18ef4e178b06a9
Hwangwonjin <ghkddnjs1269@gmail.com>
2022-08-22 19:53:22 +0900

    book1

--- book1/src/ch05/P131.java
+++ book1/src/ch05/P131.java
@@ -1,10 +1,15 @@
 package ch05;
-
+/*
+ * 날짜 : 2022/08/22
+ * 이름 : 황원진
+ * 내용 : person 클레스 만들기
+ * 
+ */
 public class P131 {
 
-	String name;
-	int height;
-	double weight;
-	char gender;
-	
+	String name;	//이름
+	int height;		//키
+	double weight;	//몸무게
+	char gender;	//성별
+	boolean married;//결혼 여부
 }
---
+++ book1/src/ch05/P132.java
@@ -0,0 +1,19 @@
+package ch05;
+/*
+ * 날짜 : 2022/08/22
+ * 이름 : 황원진
+ * 내용 : 학생 이름과 주소 출력하는 메서드 만들기
+ * 
+ */
+public class P132 {
+ int studentID;
+ String studentName;
+ int grade;
+ String address;
+ 
+ public void showStudentInfo() {
+	 System.out.println(studentName + ","+ address);
+ }
+	 
+
+}
