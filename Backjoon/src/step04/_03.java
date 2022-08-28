5d7c87e1221abb64de10c4e9332a4dfe78afb3e5
wonjin <hdigimon27@gmail.com>
2022-08-28 23:20:40 +0900

    Backjoon

--- Backjoon/src/step04/_03.java
+++ Backjoon/src/step04/_03.java
@@ -1,9 +1,17 @@
 package step04;
 
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2022/08/28
+ * 이름 : 황원진
+ * 내용 : 백준 4단계 3번 최댓값
+ * 
+ */
 public class _03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
 	}
 
