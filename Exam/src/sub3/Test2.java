dd3a083ea1bc695a657775ffdf0bd189e5a32815
Hwangwonjin <ghkddnjs1269@gmail.com>
2022-09-02 17:51:52 +0900

    Exam

---
+++ Exam/src/sub3/Test2.java
@@ -0,0 +1,37 @@
+package sub3;
+/*날짜 : 2022/09/02
+ * 이름 : 황원진
+ * 내용 : 클래스 생성자 연습문제
+ * 
+ * 
+ */
+class king{
+	private String name;
+	private int year;
+	
+	public king(int year) {
+		this("정조");
+		this.year = year;
+	}
+	
+	
+	public king(String name,int year){
+		this.name= name;
+		this.year = year;
+	}
+	
+	public king(String name){
+		this.name = name;
+	}
+	
+	
+}
+
+
+
+
+
+
+public class Test2 {
+
+}
---
+++ Exam/src/sub3/Test6.java
@@ -0,0 +1,26 @@
+package sub3;
+/*날짜: 2022/09/02
+ * 이름 : 황원진
+ * 내용: 클래스 상속 연습문제
+ * 
+ */
+class Rest{
+	public void payment() {
+		System.out.println("임내료를 받습니다.");
+	}
+}
+
+class Landload extends Rest{
+	public void getMoney() {
+		System.out.println("건물주 입니다.");
+		payment();
+	}
+}
+
+
+public class Test6 {
+	public static void main(String[] args) {
+		Landload master = new Landload();
+		master.getMoney();
+	}
+}
