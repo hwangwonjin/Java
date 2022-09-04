77f8bbfbe8c6cd9deb19349263cf53eaf85f1608
wonjin <hdigimon27@gmail.com>
2022-09-04 23:38:23 +0900

    Backjoon

--- Backjoon/src/step04/_04.java
+++ Backjoon/src/step04/_04.java
@@ -1,10 +1,35 @@
 package step04;
 
+import java.util.Scanner;
+
+/*날짜 : 2022/09/04
+ *이름 : 황원진
+ *내용 : 백준 4단계 4번 문제 평균
+ */
 public class _04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		double[] arr =  new double[a];
+		double sum = 0.0;
+		double max = arr[0];
+		for(int i = 0; i <arr.length; i++) {
+			arr[i] = sc.nextInt();
+//		}
+//		for(int i = 0; i < arr.length; i++) {
+			if(max<arr[i]) 
+				max=arr[i];		
+//		}
+		
+//		for(int i = 0; i < arr.length; i++ ) {
+//			arr[i] = arr[i]/max * 100;
+		//	System.out.println(arr[i]);
+//		}
+//		for(int i = 0; i < arr.length; i++) {
+			 sum += arr[i];
+		}
+		System.out.println(sum/a/max*100);
 	}
 
 }
--- Backjoon/src/step04/_05.java
+++ Backjoon/src/step04/_05.java
@@ -1,10 +1,32 @@
 package step04;
 
+import java.util.Scanner;
+
+/*날짜 : 2022/09/04
+ *이름 : 황원진
+ *내용 : 백준 4단계 5번 문제 OX퀴즈
+ */
 public class _05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		String[] arr = new String[a]; 
+	
+		for(int i = 0; i <a; i++) {
+			arr[i] = sc.next();
+			int sum = 0;
+			int count = 0;
+			for(int j = 0; j < arr[i].length();j++) {
+				if(arr[i].charAt(j) == 'O' ) {
+					count++;
+					sum += count;
+				}else {
+					count =0;
+				}
+			}
+			System.out.println(sum);
+		}
 	}
-
 }
+
