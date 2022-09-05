75e57b274157bede70a01f6c1ee1bbe5ed577e87
wonjin <hdigimon27@gmail.com>
2022-09-05 14:17:16 +0900

    Backjoon

--- Backjoon/src/step04/_04.java
+++ Backjoon/src/step04/_04.java
@@ -11,25 +11,17 @@ public class _04 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		double[] arr =  new double[a];
-		double sum = 0.0;
+		double[] arr = new double [a];
 		double max = arr[0];
-		for(int i = 0; i <arr.length; i++) {
+		double sum = 0;
+		for(int i = 0; i < a; i++) {
 			arr[i] = sc.nextInt();
-//		}
-//		for(int i = 0; i < arr.length; i++) {
-			if(max<arr[i]) 
-				max=arr[i];		
-//		}
+			if(max< arr[i]) {
+				max=arr[i];
+			}	
+			sum += arr[i];
 		
-//		for(int i = 0; i < arr.length; i++ ) {
-//			arr[i] = arr[i]/max * 100;
-		//	System.out.println(arr[i]);
-//		}
-//		for(int i = 0; i < arr.length; i++) {
-			 sum += arr[i];
 		}
-		System.out.println(sum/a/max*100);
+		System.out.println(sum/max*100/a);
 	}
-
 }
--- Backjoon/src/step04/_05.java
+++ Backjoon/src/step04/_05.java
@@ -7,26 +7,25 @@ import java.util.Scanner;
  *내용 : 백준 4단계 5번 문제 OX퀴즈
  */
 public class _05 {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		String[] arr = new String[a]; 
-	
-		for(int i = 0; i <a; i++) {
+		String[] arr = new String[a];
+		for(int i = 0; i < a; i++) {
 			arr[i] = sc.next();
-			int sum = 0;
 			int count = 0;
-			for(int j = 0; j < arr[i].length();j++) {
-				if(arr[i].charAt(j) == 'O' ) {
+			int sum = 0;
+			for(int j = 0; j <arr[i].length(); j++) {
+				if(arr[i].charAt(j)=='O') {
 					count++;
-					sum += count;
 				}else {
-					count =0;
+					count = 0;
 				}
+				sum += count;
 			}
 			System.out.println(sum);
 		}
-	}
+	}	
+
 }
 
