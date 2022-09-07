2c07dfc74b2510aec93f70c67035e71342903e0f
Hwangwonjin <ghkddnjs1269@gmail.com>
2022-09-07 18:00:38 +0900

    Test

---
+++ Test/src/sub1/Test.java
@@ -0,0 +1,17 @@
+package sub1;
+
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int  num = sc.nextInt();
+		for(int i = 0; i < num; i++) {
+		for(int j = 0; j <= i; j++ ) {
+			System.out.print("★");
+		}
+
+		System.out.println();
+	}
+}
+}
