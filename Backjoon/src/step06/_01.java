294bd8d93f211ace2649761178be6718dc07280b
hwangwonjin <ghkddnjs1269@gmail.com>
2022-09-18 23:21:26 +0900

    Backjoon

--- Backjoon/src/step06/_01.java
+++ Backjoon/src/step06/_01.java
@@ -1,10 +1,22 @@
 package step06;
 
+import java.util.Scanner;
+
+/*날짜 : 2022/09/18
+ *이름 : 황원진
+ *내용 : 백준 6단계 1번
+ */
 public class _01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc= new Scanner(System.in);
+		
+		char ch = sc.nextLine().charAt(0);
+		int num = (int)ch;
+		
+		System.out.println(num);
+		
 	}
 
 }
--- Backjoon/src/step06/_02.java
+++ Backjoon/src/step06/_02.java
@@ -1,10 +1,26 @@
 package step06;
 
+import java.util.Scanner;
+
+/*날짜 : 2022/09/18
+ *이름 : 황원진
+ *내용 : 백준 6단계 2번 숫자의 합
+ */
 public class _02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		String a = sc.next();
+		int sum = 0;
+	//	char ch = sc.nextLine().charAt();
+		//String[] num = Integer.toString(a).split("");
+		for(int i = 0; i <n; i++) {
+			sum += a.charAt(i)-'0';
+		}
+		System.out.println(sum);
 	}
 
 }
